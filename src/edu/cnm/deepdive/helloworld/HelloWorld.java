c1c6d4f4ab8407ff013b13a279ec272279a7c159
Michael Lacey <laceymwes@gmail.com>
2018-04-16 12:48:30 -0600

    Add HelloWorld Class

---
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.helloworld;
+
+/**
+ * Simple application that outputs the message, "Hello World!"
+ *
+ */
+public class HelloWorld {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+
+    System.out.println("Hello World!");
+
+  }
+
+}
